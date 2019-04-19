15f1c8f3cc26672112493a4a2d4911fcac6de6d0
Simone Lusetti <simemetti@gmail.com>
2019-04-19 16:33:10 +0200

    ImageSplitterCompleted

--- Neural/guess.py
+++ Neural/guess.py
@@ -5,13 +5,16 @@ import image_slicer
 network_file = open("network.pickle", "rb")
 net = pickle.load(network_file)
 
-image_slicer.slice('image.jpg', 4)
+image_slicer.slice('image.jpg', 2)
+image_slicer.slice('image_01_01.png',2)
+image_slicer.slice('image_01_02.png',2)
 
 data = []
-data.append(imgConv.imageprepare("image_01_01.png"))
-data.append(imgConv.imageprepare("image_01_02.png"))
-data.append(imgConv.imageprepare("image_02_01.png"))
-data.append(imgConv.imageprepare("image_02_02.png"))
+data.append(imgConv.imageprepare("image_01_01_01_01.png"))
+data.append(imgConv.imageprepare("image_01_01_01_02.png"))
+data.append(imgConv.imageprepare("image_01_02_01_01.png"))
+data.append(imgConv.imageprepare("image_01_02_01_02.png"))
+
 
 for single_data in data:
     for i in range(784):
